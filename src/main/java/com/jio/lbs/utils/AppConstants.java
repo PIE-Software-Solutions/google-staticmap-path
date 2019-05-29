2fb1f7b472a6490ae5e78fe4da869d81243c57ec
piesoftsol <35703117+piesoftsol@users.noreply.github.com>
2019-05-29 17:54:05 +0530

    Update AppConstants.java

--- src/main/java/com/jio/lbs/utils/AppConstants.java
+++ src/main/java/com/jio/lbs/utils/AppConstants.java
@@ -5,4 +5,6 @@ public class AppConstants {
 	public static int ZOOM_DEFAULT = 0;
 	public static int SERVICE_CONN_TIMEOUT = 408;
 	public static Integer MAX_WAY_POINTS = 23;
+	public static String SECRITE_KEY = "HiIamSecretKey!!DontSee";
+	public static String SALT_KEY = "HiIamSaltKey!!VerySalty";
 }
