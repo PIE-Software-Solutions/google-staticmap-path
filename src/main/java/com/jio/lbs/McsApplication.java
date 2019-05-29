6ae381ddd2302503dcb800580c5042e095eaee33
piesoftsol <35703117+piesoftsol@users.noreply.github.com>
2019-05-29 17:11:12 +0530

    Update McsApplication.java

--- src/main/java/com/jio/lbs/McsApplication.java
+++ src/main/java/com/jio/lbs/McsApplication.java
@@ -9,6 +9,13 @@ import org.springframework.context.annotation.Configuration;
 public class McsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(McsApplication.class, args);
+        String log4jConfigFile = System.getProperty("app.config")
+                + File.separator + "log_staticmapgen.xml";
+    	if(new File(log4jConfigFile).exists()) {
+			DOMConfigurator.configure(log4jConfigFile);
+	        SpringApplication.run(McsApplication.class, args);
+    	}else {
+    		System.out.println("Logging properties file not exists can't start server...");
+    	}
     }
 }
